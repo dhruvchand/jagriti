6a7e897b7c7c11dc9f31e2966e3abf22f8944fab
karthiksbhat <krtk_bhat@yahoo.comscript type=text/javascript(function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName(script);l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})(); /* ]] */  /script>
2014-02-28 15:50:52 +0530

    successful upload to database

--- php/verify.php
+++ php/verify.php
@@ -10,18 +10,45 @@
     // What happens when the CAPTCHA was entered incorrectly
     die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
          "(reCAPTCHA said: " . $resp->error . ")");
-  }
-  else
-  {
+  } else {
     // Your code here to handle a successful verification
+
     $address=$_POST['address'];
     $emailid=$_POST['email'];
     $description=$_POST['description'];
+    $name = $_POST['name'];
+    $number = $_POST['number'];
 
+    $username = "a2414660_jagriti";
+    $password = "projasha1234";
+
+    #Mailer without attachment
     $to="jagritiproject@gmail.com";
     $subject = "Complaint Submission";
     $body="Complaint received from $emailid .\nThe address of the location is:\n$address.\nDescription is:\n$description\n";
 
     mail($to, $subject, $body);
+
+
+    #storing in database
+    try {
+          $conn = new PDO('mysql:host=mysql3.000webhost.com;dbname=a2414660_maindb', $username, $password);
+         # $conn = new PDO('mysql:host=localhost:3036;dbname=jtest', $username, $password);
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+          # Prepare Query
+          $stmt = $conn->prepare('INSERT INTO USERS VALUES(:name, :num, :description, :email, :address)');
+          $stmt->execute(array(
+              ':name' => $name,
+              ':num' => $number,
+              ':description' => $description,
+              ':email' => $emailid,
+              ':address' => $address,
+              #':file'
+            ));
+          echo $stmt->rowCount();
+    } catch(PDOException $e) {
+        echo 'Error: ' . $e->getMessage();
+    }
   }
   ?>
